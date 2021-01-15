609962523016ab7b47b9cb6a6b9c4cfb17c12531
zhangwork1000 <zhangwork1000@gmail.com>
2021-01-15 20:18:12 +0900

    aaaa

--- src/main/java/com/evan/wj/service/UserService.java
+++ src/main/java/com/evan/wj/service/UserService.java
@@ -15,14 +15,13 @@ public class UserService {
     public User getByName(String username) {
     	User user =new User(1,username);
     	return user;
-
 //        return userDao.findByUsername(username);
     }
 
     public User get(String username, String password){
     	User user =new User(1,username);
     	return user;
-//        return userDao.getByUsernameAndPassword(username, password);
+//      return userDao.getByUsernameAndPassword(username, password);
     }
 
     public void add(User user) {
